fd11f378d483df0570170b61c97f2077f86382f2
psokolowski <psokolowski@opteam.pl>
2024-02-15 11:39:43 +0100

    add Calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
+import Calculator from './calculator/Calculator';
 
 function App() {
   return (
     <div>
       <h1>Calculator App</h1>
+
+      <Calculator />
     </div>
   );
 }
