6abcc0084d3c5d6dc65e9615ff9805abe0541551
psokolowski <psokolowski@opteam.pl>
2024-02-21 08:05:10 +0100

    fix bugs

--- src/calculator/CalculatorDisplay.tsx
+++ src/calculator/CalculatorDisplay.tsx
@@ -8,12 +8,45 @@ interface Props {
 }
 
 function CalculatorDisplay({ n1, n2, sign, result }: Props) {
+  const display = () => {
+    if (!sign && n2 === '0' && !result) {
+      return <div className='large'>{n1}</div>;
+    } else if (sign && n2 === '0' && !result) {
+      return (
+        <>
+          <div className='small'>{`${n1} ${sign}`}</div>
+          <div className='large'>{n1}</div>
+        </>
+      );
+    } else if (sign && n2 && !result) {
+      return (
+        <>
+          <div className='small'>{`${n1} ${sign}`}</div>
+          <div className='large'>{n2}</div>
+        </>
+      );
+    } else if (result) {
+      return (
+        <>
+          <div className='small'>{`${n1} ${sign} ${n2} =`}</div>
+          <div className='large'>{result}</div>
+        </>
+      );
+    } else {
+      return <div className='large'>Brak danych</div>;
+    }
+  };
+
   return (
     <div className='display'>
-      <div>n1: {n1}</div>
-      <div>sign: {sign}</div>
-      <div>n2: {n2}</div>
-      <div>result: {result}</div>
+      <div className='helper'>
+        <div className='small'>n1: {n1}</div>
+        <div className='small'>sign: {sign}</div>
+        <div className='small'>n2: {n2}</div>
+        <div className='small'>result: {result}</div>
+      </div>
+
+      <div className='preview'>{display()}</div>
     </div>
   );
 }
