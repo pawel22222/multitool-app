a4a2372fd80de474020c5767922e7e0d3ceb5e4c
psokolowski <psokolowski@opteam.pl>
2024-03-06 21:28:01 +0100

    handle overlapping windows

--- src/components/minimalizedApp/index.tsx
+++ src/components/minimalizedApp/index.tsx
@@ -2,26 +2,46 @@ import './style.scss';
 import { MouseEvent } from 'react';
 
 type Props = {
-  name: string;
-  setIsMinimalize: () => void;
+  displayName: string;
+  iconSrc: string;
+  onClick: () => void;
   closeApp: () => void;
+  isFocused: boolean;
 };
 
-export default function MinimalizedApp({ name, setIsMinimalize, closeApp }: Props) {
+export default function MinimalizedApp({
+  displayName,
+  iconSrc,
+  onClick,
+  closeApp,
+  isFocused,
+}: Props) {
   function handlerCloseApp(e: MouseEvent<HTMLButtonElement>) {
     e.stopPropagation();
     closeApp();
   }
 
+  function handleOnClick(e: MouseEvent<HTMLDivElement>) {
+    e.stopPropagation();
+    onClick();
+  }
+
   return (
     <div
-      className='minimalized-app'
-      role='button'
-      tabIndex={0}
-      onKeyDown={setIsMinimalize}
-      onClick={setIsMinimalize}
+      className={`minimalized-app ${isFocused ? 'focused' : ''}`}
+      role='presentation'
+      onMouseDown={(e) => e.stopPropagation()}
+      onClick={handleOnClick}
     >
-      <span className='label'>{name}</span>
+      <img
+        className='icon'
+        src={iconSrc || './vite.svg'}
+        alt={`${displayName} icon`}
+        width='25px'
+        height='25px'
+      />
+
+      <span className='label'>{displayName}</span>
 
       <button className='close' onClick={handlerCloseApp}>
         X
