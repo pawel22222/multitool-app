28c6134790bb45531e965f5f9af73c9f3057ec2c
psokolowski <psokolowski@opteam.pl>
2024-03-16 21:29:04 +0100

    refactor app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,181 +1,63 @@
-import { useState } from 'react';
 import './style.scss';
-import { v4 as uuidv4 } from 'uuid';
 import Calculator from './components/calculator';
 import WindowWrapper from './containers/windowWrapper';
 import { CalculatorContextProvider } from './context/CalculatorContext';
 import PulpitContainer from './containers/pulpit';
 import Nav from './containers/nav';
 import MinimalizedApp from './components/minimalizedApp';
-
-export type WindowAppTypes = 'calculator' | 'other-app';
-interface WindowProps {
-  id: string;
-  type: WindowAppTypes;
-  isFullscreen: boolean;
-  isMinimalize: boolean;
-  zIndex: number;
-}
-interface AppProps {
-  displayName: string;
-  iconSrc: string;
-  minSize: { width: string; height: string };
-}
-export interface WindowApp extends WindowProps, AppProps {}
+import { WindowApp } from './types/windowApp';
+import { useApps } from './context/AppsContext';
 
 function App() {
-  const [openedApps, setOpenedApps] = useState<WindowApp[]>([]);
-  const [focusedWindowId, setFocusedWindowId] = useState<string | null>(null);
-
-  function createApp(type: WindowAppTypes): WindowApp {
-    const biggestZIndex = getBiggestZIndex();
-
-    const appProps = ((): AppProps => {
-      switch (type) {
-        case 'calculator':
-          return {
-            iconSrc: './calc-icon.png',
-            displayName: 'Calculator',
-            minSize: { width: '420px', height: '600px' },
-          };
-        case 'other-app':
-          return {
-            iconSrc: './vite.svg',
-            displayName: 'Other App',
-            minSize: { width: '420px', height: '100px' },
-          };
-
-        default:
-          const never: never = type;
-          return never;
-      }
-    })();
-
-    const initialProps: WindowProps = {
-      id: uuidv4(),
-      type,
-      isFullscreen: false,
-      isMinimalize: false,
-      zIndex: biggestZIndex + 1,
-    };
-
-    return {
-      ...initialProps,
-      ...appProps,
-    };
-  }
-
-  function getBiggestZIndex() {
-    return openedApps.reduce((acc, { zIndex }) => {
-      if (acc < zIndex) {
-        return zIndex;
-      }
-      return acc;
-    }, 0);
-  }
-
-  function handleSetFocusedWindowId(id: string | null) {
-    increaseZIndex(id);
-    setFocusedWindowId(id);
-  }
-
-  function openApp(type: WindowAppTypes) {
-    const newApp = createApp(type);
-    setOpenedApps((prev) => [...prev, newApp]);
-    handleSetFocusedWindowId(newApp.id);
-  }
-
-  function closeApp(id: string) {
-    setOpenedApps((prev) => prev.filter((app) => app.id !== id));
-  }
-
-  function setIsMinimalize(id: string, isMinimalize: boolean) {
-    setOpenedApps((prev) => {
-      return prev.map((app) => {
-        if (app.id === id) {
-          app.isMinimalize = isMinimalize;
-
-          if (id === focusedWindowId) {
-            setFocusedWindowId(null);
-          }
-        }
-        return app;
-      });
-    });
-  }
-
-  function setIsFullscreen(id: string, isFullscreen: boolean) {
-    setOpenedApps((prev) => {
-      return prev.map((app) => {
-        if (app.id === id) {
-          app.isFullscreen = isFullscreen;
-        }
-        return app;
-      });
-    });
-  }
-
-  function increaseZIndex(id: string | null) {
-    if (!id) return;
-    const biggestZIndex = getBiggestZIndex();
-
-    setOpenedApps((prev) => {
-      return prev.map((app) => {
-        if (app.id === id) {
-          app.zIndex = biggestZIndex + 1;
-        }
-        return app;
-      });
-    });
-  }
+  const { openedApps, focusedWindowId, actions } = useApps();
 
   function renderApp(app: WindowApp) {
-    switch (app.type) {
+    const { id, type } = app;
+
+    switch (type) {
       case 'calculator':
         return (
-          <CalculatorContextProvider key={app.id}>
-            <WindowWrapper
-              windowData={app}
-              closeApp={closeApp}
-              setIsMinimalize={setIsMinimalize}
-              setIsFullscreen={setIsFullscreen}
-              isFocused={focusedWindowId === app.id}
-              handleSetFocusedWindowId={handleSetFocusedWindowId}
-            >
-              <Calculator isFocused={focusedWindowId === app.id} />
+          <CalculatorContextProvider key={id}>
+            <WindowWrapper windowApp={app} isFocused={focusedWindowId === id}>
+              <Calculator isFocused={focusedWindowId === id} />
             </WindowWrapper>
           </CalculatorContextProvider>
         );
       case 'other-app':
         return (
-          <WindowWrapper
-            key={app.id}
-            windowData={app}
-            closeApp={closeApp}
-            setIsMinimalize={setIsMinimalize}
-            setIsFullscreen={setIsFullscreen}
-            isFocused={focusedWindowId === app.id}
-            handleSetFocusedWindowId={handleSetFocusedWindowId}
-          >
+          <WindowWrapper key={id} windowApp={app} isFocused={focusedWindowId === id}>
             <div>other app</div>
           </WindowWrapper>
         );
       default:
-        const never: never = app.type;
+        const never: never = type;
         throw new Error(never);
     }
   }
+
+  const minimalizedAppOnClick = (id: string, isMinimalize: boolean) => {
+    if (isMinimalize) {
+      actions.setIsMinimalize(id, false);
+      actions.handleSetFocusedWindowId(id);
+    } else if (id === focusedWindowId) {
+      actions.setIsMinimalize(id, true);
+      actions.handleSetFocusedWindowId(null);
+    } else {
+      actions.handleSetFocusedWindowId(id);
+    }
+  };
 
   return (
     <div className='app-container light'>
       <div className='app-header'>
         <h1>Apps</h1>
+
         <Nav>
-          <button onClick={() => openApp('calculator')}>
+          <button onClick={() => actions.openApp('calculator')}>
             <img src='./calc-icon.png' alt='calculator app' width='25px' height='25px' />
           </button>
 
-          <button onClick={() => openApp('other-app')}>
+          <button onClick={() => actions.openApp('other-app')}>
             <img src='./vite.svg' alt='other app' width='25px' height='25px' />
           </button>
         </Nav>
@@ -192,18 +74,8 @@ function App() {
               key={id}
               displayName={displayName}
               iconSrc={iconSrc}
-              closeApp={() => closeApp(id)}
-              onClick={() => {
-                if (isMinimalize) {
-                  setIsMinimalize(id, false);
-                  handleSetFocusedWindowId(id);
-                } else if (id === focusedWindowId) {
-                  setIsMinimalize(id, true);
-                  handleSetFocusedWindowId(null);
-                } else {
-                  handleSetFocusedWindowId(id);
-                }
-              }}
+              closeApp={() => actions.closeApp(id)}
+              onClick={() => minimalizedAppOnClick(id, isMinimalize)}
               isFocused={focusedWindowId === id}
             />
           ))}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import AppsContextProvider from './context/AppsContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AppsContextProvider>
+      <App />
+    </AppsContextProvider>
   </React.StrictMode>,
-)
+);
